c77c4dd01c833755818f2c307b3c35441f885853
priya091 <rboopathisasikala@gmail.com>
2016-08-20 17:17:00 +0530

    Add files via upload

---
+++ rp.java
@@ -0,0 +1,22 @@
+
+
+import java.util.Scanner;
+
+
+public class Factorialaa {
+
+   
+    public static void main(String[] args) {
+     
+
+int x,fact=1;
+Scanner input=new Scanner(System.in);
+System.out.print("Enter the Integer :");
+x=input.nextInt();
+for(int i=1;i<=x;i++)
+{
+fact=fact*i;
+}
+System.out.print(fact);
+}
+}
